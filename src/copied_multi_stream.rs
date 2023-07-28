f9db45878b4c24a7a7c0458a3170a673a2f4ce93
Marcel Hauf <git@marcelhauf.name>
2023-07-28 18:33:57 +0200

    Use wakers to wake up copied streams.
    Allows async pulling of CopiedMultiStreams.

--- src/copied_multi_stream.rs
+++ src/copied_multi_stream.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    task::Waker,
+};
 
 use futures_util::{Stream, StreamExt};
 
@@ -8,6 +11,7 @@ where
     S: Stream,
 {
     cache: Box<[Option<S::Item>]>,
+    wakers: Box<[Option<Waker>]>,
     stream: Option<S>,
 }
 
@@ -24,11 +28,10 @@ where
 
 /// Copies values from the inner stream into multiple new streams. Polls from inner stream one
 /// value and waits till all new streams have pulled a copied value.
-/// Note that not pulling from all new streams in sequence will result in an endless loop
-/// polling a Pending state. Essentially blocking.
+/// Note that not pulling from all new streams will result in a blocking state.
 ///
 /// When the underlying stream terminates, all new streams which have allready pulled the last value will be [`Pending`].
-/// When all new streams have pulled the last value, all streams will terminate.
+/// When all new streams have pulled the last value, all streams will terminate on next pull.
 ///
 /// [`Pending`]: std::task::Poll#variant.Pending
 pub fn copied_multi_stream<S>(stream: S, i: usize) -> Vec<CopiedMultiStream<S>>
@@ -38,6 +41,7 @@ where
     let state = Arc::new(Mutex::new(CopiedMultiStreamState {
         stream: Some(stream),
         cache: (0..i).map(|_| None).collect(),
+        wakers: (0..i).map(|_| None).collect(),
     }));
     (0..i)
         .map(|pos| CopiedMultiStream {
@@ -62,18 +66,32 @@ where
         if let Some(v) = state.cache[self.pos].take() {
             std::task::Poll::Ready(Some(v))
         } else if state.cache.iter().any(Option::is_some) {
+            state.wakers[self.pos] = Some(cx.waker().clone());
             std::task::Poll::Pending
         } else if let Some(ref mut stream) = state.stream {
             match stream.poll_next_unpin(cx) {
                 std::task::Poll::Ready(Some(v)) => {
                     state.cache.iter_mut().for_each(|c| *c = Some(v.clone()));
+                    state.wakers.iter_mut().for_each(|waker| {
+                        if let Some(waker) = waker.take() {
+                            waker.wake_by_ref()
+                        }
+                    });
                     std::task::Poll::Ready(state.cache[self.pos].take())
                 }
                 std::task::Poll::Ready(None) => {
                     state.stream = None;
+                    state.wakers.iter_mut().for_each(|waker| {
+                        if let Some(waker) = waker.take() {
+                            waker.wake_by_ref()
+                        }
+                    });
                     std::task::Poll::Ready(None)
                 }
-                std::task::Poll::Pending => std::task::Poll::Pending,
+                std::task::Poll::Pending => {
+                    state.wakers[self.pos] = Some(cx.waker().clone());
+                    std::task::Poll::Pending
+                }
             }
         } else {
             std::task::Poll::Ready(None)
@@ -86,6 +104,7 @@ mod tests {
     use std::pin::pin;
 
     use futures_util::stream::{self, BoxStream};
+    use ntest_timeout::timeout;
 
     use crate::StreamUtils;
 
@@ -122,7 +141,8 @@ mod tests {
         assert_eq!(res.len(), size);
         let res = stream::select_all(res);
         let res: Vec<usize> = res.collect().await;
-        assert_eq!(res, vec![]);
+        let exp: Vec<usize> = Vec::new();
+        assert_eq!(res, exp);
     }
 
     #[tokio::test]
@@ -133,7 +153,8 @@ mod tests {
         assert_eq!(res.len(), size);
         let res = stream::select_all(res);
         let res: Vec<usize> = res.collect().await;
-        assert_eq!(res, vec![]);
+        let exp: Vec<usize> = Vec::new();
+        assert_eq!(res, exp);
     }
 
     #[tokio::test]
@@ -154,5 +175,22 @@ mod tests {
         let res = stream::select_all(res);
         let res: Vec<usize> = res.collect().await;
         assert_eq!(res, vec![0, 0, 0, 2, 2, 2, 4, 4, 4]);
+    }
+
+    #[tokio::test]
+    #[timeout(200)]
+    async fn test_async_pull() {
+        let size = 5;
+        let stream = stream::iter(0..3);
+        let res = stream.copied_multi_stream(size);
+
+        let res: Vec<_> = res
+            .into_iter()
+            .map(|stream| tokio::task::spawn(async move { stream.collect::<Vec<usize>>().await }))
+            .collect();
+        for r in res {
+            let r = r.await.unwrap();
+            assert_eq!(r, vec![0, 1, 2]);
+        }
     }
 }
